25f925ded09937dd563aee3491016472b7e18aba
Marc <kemar79@qq.com>
2025-10-17 17:19:19 +0100

    implement navigation bar #5

--- app/src/main/java/com/marcal/recordkeeper/MainActivity.kt
+++ app/src/main/java/com/marcal/recordkeeper/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.marcal.recordkeeper
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.commit
+import com.google.android.material.navigation.NavigationBarView
 import com.marcal.recordkeeper.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,9 +16,32 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        supportFragmentManager.commit {
-            add(R.id.frame_content, RunningFragment())
-        }
+        binding.bottomNav.setOnItemSelectedListener(this)
+    }
 
+    private fun onCyclingClick() {
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, CyclingFragment())
+        }
+    }
+
+    private fun onRunningClick() {
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, RunningFragment())
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.nav_running) {
+            onRunningClick()
+            return true
+        } else if (item.itemId == R.id.nav_cycling) {
+            onCyclingClick()
+            return true
+        } else {
+            return false
+        }
     }
 }
+
+
