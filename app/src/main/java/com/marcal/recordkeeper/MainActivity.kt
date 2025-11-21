321afa38c136123cca6e5cf310329746387312fe
Marc <kemar79@qq.com>
2025-11-21 15:11:34 +0000

    add option button #14

--- app/src/main/java/com/marcal/recordkeeper/MainActivity.kt
+++ app/src/main/java/com/marcal/recordkeeper/MainActivity.kt
@@ -3,6 +3,7 @@ package com.marcal.recordkeeper
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import com.google.android.material.navigation.NavigationBarView
@@ -24,6 +25,29 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
         menuInflater.inflate(R.menu.toolbar, menu)
         return true
     }
+
+    override fun onOptionsItemSelected(item: MenuItem)= when (item.itemId) {
+            R.id.reset_running -> onRestRunningMenuOptionClick(this)
+            R.id.reset_cycling -> onRestCyclingMenuOptionClick(this)
+            R.id.reset_all -> onRestAllMenuOptionClick(this)
+            else -> super.onOptionsItemSelected(item)
+    }
+
+
+    private fun onRestRunningMenuOptionClick(activity: MainActivity): Boolean {
+        Toast.makeText(activity, "Clear all Runs", Toast.LENGTH_SHORT).show()
+        return true
+    }
+    private fun onRestCyclingMenuOptionClick(activity: MainActivity): Boolean {
+        Toast.makeText(activity, "Clear all cycling", Toast.LENGTH_SHORT).show()
+        return true
+    }
+
+    private fun onRestAllMenuOptionClick(activity: MainActivity): Boolean {
+        Toast.makeText(activity, "Clear all", Toast.LENGTH_SHORT).show()
+        return true
+    }
+
 
     private fun onCyclingClick(): Boolean {
         supportFragmentManager.commit {
