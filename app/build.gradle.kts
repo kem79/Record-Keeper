9de730750deca597badae34921f76260eeb76a6a
Marc <kemar79@qq.com>
2025-11-22 11:30:00 +0000

    try 3 types of Preferences #18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.preference.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(libs.androidx.fragment.ktx)
 }
--- app/src/main/java/com/marcal/recordkeeper/running/EditRunningRecordActivity.kt
+++ app/src/main/java/com/marcal/recordkeeper/running/EditRunningRecordActivity.kt
@@ -1,7 +1,10 @@
 package com.marcal.recordkeeper.running
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
+import androidx.preference.PreferenceManager
 import com.marcal.recordkeeper.databinding.ActivityEditRunningRecordBinding
 
 class EditRunningRecordActivity : AppCompatActivity() {
@@ -14,5 +17,28 @@ class EditRunningRecordActivity : AppCompatActivity() {
         setContentView(binding.root)
         val distance = intent.getStringExtra("Distance")
         title = "$distance Record"
+
+        // default preference file without name (name is fully qualified package name + _preferences.xml
+        // it s application wide preferences file
+        val applicationPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        applicationPreferences.edit {
+            putString("My key value here", "My value stored in default shared preferences")
+        }
+
+        // activity preferences (1 file per activity)
+        // 1 file per activity
+        // package name + activity name + .xml
+        // seldom use, only if your activity requires a config specific to theis view
+        val activityPreferences = getPreferences(Context.MODE_PRIVATE)
+        activityPreferences.edit {
+            putInt("some data key for my activity", 15)
+        }
+
+        // shared preferences (multiple preferences files)
+        // arguably the most useful
+        val activitySharedPref = getSharedPreferences("my activity file name", Context.MODE_PRIVATE)
+        activitySharedPref.edit {
+            putBoolean("my data key", true)
+        }
     }
 }
