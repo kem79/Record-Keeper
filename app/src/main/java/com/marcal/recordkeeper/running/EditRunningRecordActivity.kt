3631561e6d1f08e1241351bd74e86b51d0062ba0
Marc <kemar79@qq.com>
2025-11-22 14:16:55 +0000

    add delete record function #23

--- app/src/main/java/com/marcal/recordkeeper/running/EditRunningRecordActivity.kt
+++ app/src/main/java/com/marcal/recordkeeper/running/EditRunningRecordActivity.kt
@@ -1,7 +1,6 @@
 package com.marcal.recordkeeper.running
 
 import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
@@ -11,20 +10,16 @@ import com.marcal.recordkeeper.databinding.ActivityEditRunningRecordBinding
 class EditRunningRecordActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditRunningRecordBinding
-    private lateinit var runningSharedPref: SharedPreferences
+    private val runningSharedPref by lazy { getSharedPreferences("running_preferences", Context.MODE_PRIVATE) }
     private val distance: String? by lazy { intent.getStringExtra("Distance") }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditRunningRecordBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        title = "$distance Record"
 
+        setupUi()
         displayRecord()
-        binding.buttonSave.setOnClickListener {
-            saveRecord()
-            finish()
-        }
 
         // default preference file without name (name is fully qualified package name + _preferences.xml
         // it s application wide preferences file
@@ -50,17 +45,28 @@ class EditRunningRecordActivity : AppCompatActivity() {
         }
     }
 
+    private fun setupUi() {
+        title = "$distance Record"
+        binding.buttonSave.setOnClickListener {
+            saveRecord()
+            finish()
+        }
+        binding.buttonDelete.setOnClickListener {
+            deleteRecord()
+            finish()
+        }
+    }
+
+    private fun deleteRecord() {
+        runningSharedPref.edit {
+            remove("$distance record")
+            remove("$distance date")
+        }
+    }
+
     private fun displayRecord() {
-        binding.editTextRecord
-            .setText(
-                getSharedPreferences("running_preferences", Context.MODE_PRIVATE)
-                    .getString("$distance record", null)
-            )
-        binding
-            .editTextDate.setText(
-                getSharedPreferences("running_preferences", Context.MODE_PRIVATE)
-                    .getString("$distance date", null)
-            )
+        binding.editTextRecord.setText(runningSharedPref.getString("$distance record", null))
+        binding.editTextDate.setText(runningSharedPref.getString("$distance date", null))
     }
 
     private fun saveRecord() {
@@ -73,7 +79,6 @@ class EditRunningRecordActivity : AppCompatActivity() {
 //        // commit changes to the shared pref file
 //        editor.apply()
 
-        runningSharedPref = getSharedPreferences("running_preferences", Context.MODE_PRIVATE)
         runningSharedPref.edit {
             putString("$distance record", record)
             putString("$distance date", date)
